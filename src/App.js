099e8607a314d210079213a240c96478cfcd81e8
pksingh <prashantksingh@beingzero.in>
2025-02-12 20:58:50 +0530

    Integrated with API

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
+import React, {useState, useEffect} from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [apiResponse, setApiResponse] = useState("Loading...!");
+
+  const fetchData = () => {
+    setApiResponse("Loading...!");
+
+    fetch("https://s52ww2xtz0.execute-api.us-west-2.amazonaws.com/dev/bz-test-api-12-feb")
+    .then((response) => response.json())
+    .then((data) => {
+      setApiResponse(JSON.stringify(data, null, 2));
+    })
+    .catch((err) => {
+      console.error("Error occured");
+      setApiResponse("Having issues with API call");
+    })
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to boot.
+        <p>Being Zero first session is about to start. But before that...
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <pre>{apiResponse}</pre>
+      
+        <button onClick={fetchData} style={{padding: "10px", cursor: "pointer"}}>
+          Click here
+        </button>
+
+
       </header>
     </div>
   );
